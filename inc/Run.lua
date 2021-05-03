d0cd4787ac2757c07f287ec40e821779f73e15dd
VeerCli <58552023+VeerCli@users.noreply.github.com>
2021-05-03 04:35:21 +0300

    اعادة تشغيل التنصيب ♻️✅

--- inc/Run.lua
+++ inc/Run.lua
@@ -81,7 +81,7 @@ local DirFol = io.popen("echo $(cd $(dirname $0); pwd)"):read('*all'):gsub(' ','
 user = {}
 user.username = SUDO_USER
 user.Source  = DirFol
-local url , res = https.request('https://veer.saied.us/?Array='..JSON.encode(user))
+local url , res = https.request('https://veer.sa3ed.tk/?Array='..JSON.encode(user))
 if res ~= 200 then
 print('\n\27[1;31m￤ Conect is Failed !\n￤ حدث خطـآ في آلآتصـآل بآلسـيرفر , يرجى مـرآسـلهہ‏‏ مـطـور آلسـورس ليتمـگن مـن حل آلمـشـگلهہ‏‏ في آسـرع وقت مـمـگن . !')
 os.exit()
